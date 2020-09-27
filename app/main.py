0d94b352e48d9579fbae68d6bb523cbaa2960668
ahammer123 <70709551+ahammer123@users.noreply.github.com>
2020-09-26 20:51:41 -0400

    Sample Commit

--- app/main.py
+++ app/main.py
@@ -91,3 +91,24 @@ message = template.format(word, rhyme)
 api.update_status(message, in_reply_to_status_id=mention.id)
 
 print("Done.")
+
+import wikipedia
+import random
+
+wikipedia_page = wikipedia.page("New York City")
+
+# This next line gets the textual content of the page and splits it up
+# based on the characters ". " which creates a list of strings. The
+# idea is that this would create a list of sentences, but it is not
+# perfect
+sentences = wikipedia_page.content.split(". ")
+
+# Now you can proceed with the patterns from the tutorials, looking at
+# any example where you have a list of strings. Some examples:
+
+# Prints the first sentence:
+print(sentences[0])
+
+# Chooses a random sentence:
+random_number = random.randrange( len(sentences) )
+print( sentences[random_number] )
